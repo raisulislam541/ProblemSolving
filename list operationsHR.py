25deb09daafc89448bd9e6b7b4cf2e0d7de55caf
Sajal <raisulislam541@gmail.com>
2021-02-03 22:31:10 +0600

    [add][encryption technique]

--- list operationsHR.py
+++ list operationsHR.py
@@ -50,3 +50,21 @@ sub_string = input().strip()
 
 p = count_substring(string=string, sub_string=sub_string)
 print(p)
+
+from cryptography.fernet import Fernet
+key = 'vuJq7yG972ty1P5bGttUn5TFYmfTo8vnL7wECo9a3lk='
+cipher_suite = Fernet(key)
+encoded_text = cipher_suite.encrypt(b"PbLCh@")
+decoded_text = cipher_suite.decrypt(encoded_text).decode('utf-8')
+print(encoded_text, decoded_text)
+
+from decouple import config
+from cryptography.fernet import Fernet
+
+key = config('KEY')
+DB_PASS = config("DB_PASSWORD")
+
+cipher_suite = Fernet(key)
+decoded_text = cipher_suite.decrypt(DB_PASS.encode('utf-8')).decode('utf-8')
+url = f"postgres://{config('DB_USER')}:{decoded_text}@{config('DB_HOST')}:5432/{config('DB_NAME')}"
+print(url)

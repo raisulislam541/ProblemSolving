023776741871a3b2b9c7e67fbbae3202e3578b8b
MD. Raisul Islam <raisulislam541@gmail.com>
2021-07-19 18:21:33 +0600

    [add][full decorator]

--- decorator.py
+++ decorator.py
@@ -1,26 +1,47 @@
 # decorator
 
 
-def welcome_user(func):
-    user = input("enter your  user name: ")
-    print(f"hello, {user}")
-    func()  # calling the referenced function
+# def welcome_user(func):
+#     user = input("enter your  user name: ")
+#     print(f"hello, {user}")
+#     func()  # calling the referenced function
+#
+#
+# def say_hello():
+#     print("welcome to my app")
+#
+#
+# welcome_user(say_hello)  # sending the reference to the function
+#
+# # incomplete decorators
+# def decorator():
+#     def say_hello():
+#         print("Welcome to my app")
+#
+#     return say_hello
+#
+#
+# my_func = decorator()
+# print(my_func)   # printing my_func will just show the reference  object
+# my_func()  # will print the actual function
 
 
-def say_hello():
-    print("welcome to my app")
+import functools
 
 
-welcome_user(say_hello)  # sending the reference to the function
-
-# incomplete decorators
-def decorator():
+def new_decorator(func):
+    @functools.wraps(func)
     def say_hello():
         print("Welcome to my app")
+        func()
 
     return say_hello
 
 
-my_func = decorator()
-print(my_func)   # printing my_func will just show the reference  object
-my_func()  # will print the actual function
+@new_decorator
+def welcome_user():
+    user = input("enter your  user name: ")
+    print(f"hello, {user}")  # calling the referenced function
+
+
+welcome_user()
